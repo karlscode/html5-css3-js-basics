977ff36678f10b3a033d5c2efbb4430028d845e6
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-04-12 01:24:34 -0300

    Add final adjustments.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const nunjucks = require("nunjucks")
 
 const server = express()
 const videos = require("./database/data")
+const about = require("./database/about")
 
 server.use(express.static("../public"))
 
@@ -15,17 +16,6 @@ nunjucks.configure("../views", {
 })
 
 server.get("/", function(req, resp) {
-    const about = {
-        avatar_url: "https://avatars3.githubusercontent.com/u/26313317?s=460&v=4",
-        name: "Carlos Anderson",
-        role: "FullStack Developer",
-        description: "Focused on technologies for building systems from Backend to Frontend. ",
-        links: [
-            {name: "GitHub", url: "https://github.com/karlscode"},
-            {name: "LinkedIn", url: "https://www.linkedin.com/in/carlos-anderson-ti/"}
-        ]
-    }
-
     return resp.render("about", { about })
 })
 
