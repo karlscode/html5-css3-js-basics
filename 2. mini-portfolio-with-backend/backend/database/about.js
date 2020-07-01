e74ed17395326abeae290e368c672198825423c4
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-06-30 23:28:12 -0300

    feat: Add mini-portfolio-with-backend project.

---
+++ 2. mini-portfolio-with-backend/backend/database/about.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    avatar_url: "https://avatars3.githubusercontent.com/u/26313317?s=460&v=4",
+    name: "Carlos Anderson",
+    role: "FullStack Developer",
+    description: "Focused on technologies for building systems from Backend to Frontend. ",
+    links: [
+        {name: "GitHub", url: "https://github.com/karlscode"},
+        {name: "LinkedIn", url: "https://www.linkedin.com/in/carlos-anderson-ti/"}
+    ]
+}
